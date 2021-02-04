0f73f68e104652779feb7276a5a0cf6494b4e9ca
Soohyun Kim <shkim@olulo.io>
2021-02-04 23:03:42 +0900

    #2. 스타일링 전, 데이터 조회 성공

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React from "react"
-import PropTypes from "prop-types"
+import axios from "axios"
+import Movie from "./Movie"
+import "./Movie.css"
 
 class App extends React.Component {
-  
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async() => {
+    const {
+      data: { 
+          data: { 
+            movies 
+        }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() {
+    this.getMovies()  
+  }
+  render() {
+    const  { isLoading, movies } = this.state;
+    return <section class="container">
+      {isLoading
+       ? (
+        <div class="loader">
+          <span class="loader__text">Loading</span>
+        </div>
+       ) : (
+        <div class="movies">
+          {
+            movies.map(movie => {
+              return <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+              />
+            }) 
+          }
+        </div>
+       )
+    }</section>;
+  }
 }
 
 export default App;
